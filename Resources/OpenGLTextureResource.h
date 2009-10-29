668d08c05f95e207fda49270f0347ff30880cf09
asgerhoedt <asgerhoedt@gmail.com>
2009-10-29 16:56:50 +0100

    Updated OpenGLTextureRes to more than a stub implementation
    darcs-hash:20091029155650-ceda8-ef5c64e98154cfacb69f8feea47afe19375085ce.gz

--- Resources/OpenGLTextureResource.h
+++ Resources/OpenGLTextureResource.h
@@ -18,18 +18,27 @@ namespace OpenEngine {
         class OpenGLTextureResource : public ITextureResource {
         private:
             GLuint id;
+            unsigned int width, height, depth;
         public:
-            OpenGLTextureResource(GLuint id) : id(id) {}
+            OpenGLTextureResource(GLuint id, unsigned int w, unsigned int h, unsigned int d = 24)
+                : id(id), width(w), height(h), depth(d) {
+            }
             ~OpenGLTextureResource() {}
             void Load() {}
             void Unload() {}
             int GetID() { return id; }
             void SetID(int id) { throw Exception("OpenGL textures can not change identifiers."); }
-            unsigned int GetWidth() { return 400; }
-            unsigned int GetHeight() { return 300; }
-            unsigned int GetDepth() { return 32; }
+            unsigned int GetWidth() { return width; }
+            unsigned int GetHeight() { return height; }
+            unsigned int GetDepth() { return depth; }
             unsigned char* GetData() { throw Exception("Cannot extract data from OpenGL."); }
-            ColorFormat GetColorFormat() { return RGBA; }
+            ColorFormat GetColorFormat() { 
+                switch (depth){
+                case 8: return LUMINANCE; break;
+                case 24: return RGB; break;
+                case 32: return RGBA; break;
+                }
+            }
         };
         
     }
