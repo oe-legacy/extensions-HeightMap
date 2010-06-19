3f2c9d612b2f7b6c861464dbf885cd1fc69e0451
asgerhoedt <asgerhoedt@gmail.com>
2010-06-19 12:35:52 +0200

    Small fixes to sunnodes time of day.
    Ignore-this: 1d53e3e5e6075d887587530f2333a6db
    
    darcs-hash:20100619103552-ceda8-54aaac37104fb839d1b7a145f68611222461525f.gz

--- Scene/SunNode.cpp
+++ Scene/SunNode.cpp
@@ -1,6 +1,6 @@
 // Sun node.
 // -------------------------------------------------------------------
-// Copyright (C) 2007 OpenEngine.dk (See AUTHORS) 
+// Copyright (C) 2010 OpenEngine.dk (See AUTHORS) 
 // 
 // This program is free software; It is covered by the GNU General 
 // Public License version 2 or any later version. 
@@ -9,7 +9,6 @@
 
 #include <Scene/SunNode.h>
 #include <Math/Math.h>
-#include <Logging/Logger.h>
 
 using namespace std;
 
@@ -33,43 +32,43 @@ namespace OpenEngine {
             direction = dir;
             origo = o;
             time = 0;
-            dayLength = 5000000;
+            dayLength = 50000000;
             Move(time);
         }
 
         void SunNode::SetTimeOfDay(const float t){
-            time = t-6;
+            time = (t-6) / 12.0f;
         }
         
         float SunNode::GetTimeOfDay() const {
-            return time + 6.0;
+            return fmod(time * 12.0f + 6.0f, 24.0f);
         }
 
         void SunNode::Move(unsigned int dt){
-            time += float(dt) / dayLength;
             if (dayLength != 0){
-                float cosine = cos(time / 12.0f * Math::PI);
-                float sinus = sin(time / 12.0f * Math::PI);
-                
-                // move the sun
-                coords[0] = origo[0] + direction[0] * cosine;
-                coords[1] = origo[1] + direction[1] * sinus;
-                coords[2] = origo[2] + direction[2] * cosine;
-                
-                // set the diffuse strength
-                if (sinus <= -0.1)
-                    diffuse = Vector<4, float>((float)0);
-                else if (sinus < 0.15)
-                    diffuse = baseDiffuse * (sinus + 0.1) * 4;
-                else 
-                    diffuse = baseDiffuse;
-                
-                // set the specular strength
-                if (sinus <= -0.05)
-                    specular = Vector<4, float>(0.0);
-                else
-                    specular = baseSpecular;
+                time += float(dt) / dayLength;
             }
+            float cosine = cos(time * Math::PI);
+            float sinus = sin(time * Math::PI);
+            
+            // move the sun
+            coords[0] = origo[0] + direction[0] * cosine;
+            coords[1] = origo[1] + direction[1] * sinus;
+            coords[2] = origo[2] + direction[2] * cosine;
+                
+            // set the diffuse strength
+            if (sinus <= -0.1)
+                diffuse = Vector<4, float>((float)0);
+            else if (sinus < 0.15)
+                diffuse = baseDiffuse * (sinus + 0.1) * 4;
+            else 
+                diffuse = baseDiffuse;
+                
+            // set the specular strength
+            if (sinus <= -0.05)
+                specular = Vector<4, float>(0.0);
+            else
+                specular = baseSpecular;
         }
 
         void SunNode::VisitSubNodes(ISceneNodeVisitor& visitor){
